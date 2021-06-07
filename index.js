b505ca3b370a34eb45ead6840f380fc0b2459a8f
xanf-code <darshanaswath@gmail.com>
2021-06-07 12:52:42 +0530

    login

--- index.js
+++ index.js
@@ -1,36 +1,69 @@
 const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
 
+// VARIABLES
+const url = 'https://www.amazon.in/Wowheads-Handmade-Fragile-Lightsaber-Figurine/dp/B08B43H46P/';
+const myPrice = 700;
 //Timer
 async function sleep(miliseconds) {
     return new Promise(resolve => setTimeout(resolve, miliseconds));
 }
 
 // Main Function
-(async () => {
+async function configure() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage()
+    await page.goto(url);
+    await sleep(3000);
+    checkPrice(page);
+}
+async function checkPrice(page) {
+    try {
+        await page.reload();
+        let html = await page.evaluate(() => document.body.innerHTML);
+        const $ = cheerio.load(html);
+        $('#priceblock_ourprice', html).each(function () {
+            let price = $(this).text();
+            let currentPrice = Number(price.replace(/[^0-9.-]+/g, ""));
+
+            if (currentPrice > myPrice) {
+                login(page).then(() => {
+                    buyProduct(page);
+                });
+                // #buy-now-button
+            }
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function login(page) {
     // CHECK LOGIN
-    await page.goto('https://www.amazon.in/');
-    sleep(1000);
+    await sleep(1000);
     await page.click('#nav-link-accountList');
     sleep(2000);
     await page.waitForSelector('#ap_email')
     await page.type('#ap_email', 'darshanaswath@gmail.com');
-    sleep(2000);
+    await sleep(2000);
     await page.click('#continue');
-    sleep(3000);
+    await sleep(3000);
     await page.waitForSelector('#ap_password');
     // ADD PASSWORD AS ENVIRONMENT VARIABLE
     await page.type('#ap_password', '91138817455579');
-    sleep(3000);
+    await sleep(3000);
     await page.waitForSelector('#signInSubmit');
-    sleep(3000);
+    await sleep(3000);
     await page.click('#signInSubmit');
-    sleep(5000);
-    // NAVIGATE TO YOUR PRODUCT PAGE
-    await browser.newPage()
-    await page.goto('https://www.amazon.in/Wowheads-Handmade-Fragile-Lightsaber-Figurine/dp/B08B43H46P/ref=sr_1_1?dchild=1&keywords=yoda&qid=1623045988&sr=8-1');
-    sleep(3000);
+    await sleep(3000);
+}
 
-    // await browser.close()
-})()
+async function buyProduct(page) {
+
+}
+
+async function run() {
+    await configure();
+}
+
+run();
